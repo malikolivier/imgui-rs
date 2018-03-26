643e3505889e04e926adc770491e767ebea9f2d2
Malik Olivier Boussejra <malik@boussejra.com>
2018-03-26 13:38:24 +0900

    imgui: Ui: Add is_item_active

--- src/lib.rs
+++ src/lib.rs
@@ -1107,4 +1107,11 @@ impl<'ui> Ui<'ui> {
     pub fn is_item_hovered(&self) -> bool {
         unsafe { sys::igIsItemHovered(ImGuiHoveredFlags::empty()) }
     }
+
+    /// Returns `true` if the last item is being active.
+    pub fn is_item_active(&self) -> bool {
+        unsafe {
+            sys::igIsItemActive()
+        }
+    }
 }
