655eedf2560492f3dcf704831ae9d29d45f93a48
Malik Olivier Boussejra <malik@boussejra.com>
2018-10-09 19:29:43 +0900

    [examples] support: Use imgui-glutin-support

--- imgui-examples/examples/hello_world.rs
+++ imgui-examples/examples/hello_world.rs
@@ -2,6 +2,7 @@ extern crate glium;
 #[macro_use]
 extern crate aflak_imgui as imgui;
 extern crate aflak_imgui_glium_renderer as imgui_glium_renderer;
+extern crate imgui_glutin_support;
 
 use imgui::*;
 
